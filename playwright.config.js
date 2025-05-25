94166ee077d4967ab6b321a91d89722354c69138
Muhtar <Muhtar.Mahmut0519@gmail.com>
2025-05-25 11:11:10 -0400

    jenkins-practice

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,10 @@ const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
   testDir: "./tests",
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: 5,
   reporter: "html",
   use: {
     trace: "on-first-retry",
